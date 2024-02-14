0819d5ce97a529bc83a89c32fc2bae307127e7a8
soknem <sokname56@gmail.com>
2024-02-14 15:10:22 +0700

    WatiDemo code

--- src/threadp1/WaitDemo.java
+++ src/threadp1/WaitDemo.java
@@ -1,29 +1,80 @@
 package threadp1;
-
-class Data{
+// async/await vs thread ... !
+// Consumer - Producer Pattern
+class Data {
     int msg;
-    boolean valueSet=false;
-    public synchronized void get(){
+    boolean valueSet = false;
+    public synchronized void set(int msg) {
         try {
             Thread.sleep(500);
-        }catch (Exception e){}
-        while (!valueSet){
+        } catch (Exception e) {
+        }
+        while (valueSet) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
 
         }
-        System.out.println("Consume: "+msg);
-        valueSet=false;
+        this.msg = msg;
+        System.out.println("Produce : "+msg);
+        valueSet = true;
+        notify();
     }
-    public synchronized void set(int msg){
-        try{
-            Thread.sleep(500);
-        }catch (Exception e){}
-        while (!valueSet){
-            try{
 
-            }catch (Exception e){}
+    public synchronized void get() {
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
         }
 
+        while (!valueSet) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+        System.out.println("Consume:  " + msg);
+        valueSet = false;
+        notify();
     }
 }
+
+
 public class WaitDemo {
+    public static void main(String[] args) {
+        Data data = new Data();
+
+        Thread producer = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    data.set(i);
+                }
+            }
+        };
+        Thread consumer = new Thread(){
+            @Override
+            public void run() {
+                for(int i =0; i <5; i++){
+                    data.get();
+                }
+
+            }
+        };
+
+        producer.start();
+        consumer.start();
+
+        // wait for the thread to complete the execution
+        try{
+            producer.join();
+            consumer.join();
+
+            System.out.println("Operation completed..........");
+
+        }catch (Exception ex){}
+
+
+    }
 }
