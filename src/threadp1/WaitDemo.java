40bbb363591155f539db6e2e05419695252231d2
soknem <sokname56@gmail.com>
2024-02-14 15:06:13 +0700

    thread

--- src/threadp1/WaitDemo.java
+++ src/threadp1/WaitDemo.java
@@ -1,4 +1,29 @@
 package threadp1;
 
+class Data{
+    int msg;
+    boolean valueSet=false;
+    public synchronized void get(){
+        try {
+            Thread.sleep(500);
+        }catch (Exception e){}
+        while (!valueSet){
+
+        }
+        System.out.println("Consume: "+msg);
+        valueSet=false;
+    }
+    public synchronized void set(int msg){
+        try{
+            Thread.sleep(500);
+        }catch (Exception e){}
+        while (!valueSet){
+            try{
+
+            }catch (Exception e){}
+        }
+
+    }
+}
 public class WaitDemo {
 }
